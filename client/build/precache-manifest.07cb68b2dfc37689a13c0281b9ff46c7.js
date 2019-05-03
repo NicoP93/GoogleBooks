0fcd734c58250221c005a4e6f0d8eaaa4c324a95
NicoP93 <nicopcrr@gmail.com>
2019-05-02 18:55:53 -0600

    base files, react with mongoose/express

---
+++ client/build/precache-manifest.07cb68b2dfc37689a13c0281b9ff46c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e3ef156a16b054faeb1",
+    "url": "/static/css/main.6de3a2b8.chunk.css"
+  },
+  {
+    "revision": "3e3ef156a16b054faeb1",
+    "url": "/static/js/main.f857985f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "f6cc2da088ba66b6e604061a6211a33c",
+    "url": "/index.html"
+  }
+];
